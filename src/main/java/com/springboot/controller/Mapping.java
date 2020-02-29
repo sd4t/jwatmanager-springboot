3e7b8c3afcb8eb9d0f2a9b4550b05dff0cfe126a
tuanth1479 <tuanth.nc.uit@gmail.com>
2020-02-29 09:15:11 +0700

    Add live load, apply changes without reload server many times

---
+++ src/main/java/com/springboot/controller/Mapping.java
@@ -0,0 +1,22 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Mapping {
+
+	@GetMapping("/")
+	public String index(){
+		return "index";
+	}
+
+	@GetMapping("/login")
+	public String login(){
+		return "login";
+	}
+	@GetMapping("/register")
+	public String register(){
+		return "register";
+	}
+}
