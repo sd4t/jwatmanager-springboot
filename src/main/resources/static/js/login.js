67db179e5c17a560af4a0b5ff1d4a9500054547c
tuanth1479 <tuanth.nc.uit@gmail.com>
2020-03-11 17:05:00 +0700

    addition changes at company

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,10 +1,12 @@
 async function login() {
-    var data = $("form").serializeArray();
-    console.log(data[1].value);
+    // var data = $("form").serializeArray();
 
     axios.post('/login', {
-        email: data[0].value,
-        password: data[1].value
+        // email: data[0].value,
+        // password: data[1].value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+
     })
         .then(function (response) {
             console.log(response);
