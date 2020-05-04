45d715ea1d18c39d98444aa9a3379bd2c5df44f9
tuanth1479 <tuanth.nc.uit@gmail.com>
2020-05-04 17:25:15 +0700

    I commit

--- src/main/java/com/jwat/controller/MainController.java
+++ src/main/java/com/jwat/controller/MainController.java
@@ -3,11 +3,11 @@ package com.jwat.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MainController {
-	@GetMapping("/index")
-	public String index() {
-		System.out.println("Index here");
-		return "Return home page if you've logined, login page if not";
-	}
-}
+// @RestController
+// public class MainController {
+// 	@GetMapping("/index")
+// 	public String index() {
+// 		System.out.println("Index here");
+// 		return "Return home page if you've logined, login page if not";
+// 	}
+// }
--- src/main/java/com/jwat/controller/TestUiController.java
+++ src/main/java/com/jwat/controller/TestUiController.java
@@ -5,11 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestUiController {
-    @GetMapping("/")
-    public String home() {
-        return "index";
-    }
-
+    
     @GetMapping("/login")
     public String login(){
         return "login";
